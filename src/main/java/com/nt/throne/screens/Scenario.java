a863ae5e897d9bb32e2026d95380fb60c83ed4b5
Oscar Andres Gomez <os.gomez.lozano@gmail.com>
2023-06-08 16:04:57 -0500

    Compiling state
    # Conflicts:
    #	src/main/java/com/nt/throne/model/ChaserEnemy.java
    #	src/main/java/com/nt/throne/screens/Scenario.java

--- src/main/java/com/nt/throne/screens/Scenario.java
+++ src/main/java/com/nt/throne/screens/Scenario.java
@@ -7,8 +7,12 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
 import java.awt.*;
+import java.io.File;
 import java.util.Random;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -25,16 +29,19 @@ public abstract class Scenario extends BaseScreen {
     private Random random;
     private boolean shooting;
     private boolean movingEnemies;
+    private final MediaPlayer bodyImpactSound;
+    private final MediaPlayer blockImpactSound;
 
     public Scenario(Canvas canvas, Image background) {
         super(canvas);
+        bodyImpactSound = new MediaPlayer(new Media(new File(System.getProperty("user.dir") + "/src/main/resources/com/nt/throne/Audio/GameSong/bodyImpactSound.mp3").toURI().toString()));
+        blockImpactSound = new MediaPlayer(new Media(new File(System.getProperty("user.dir") + "/src/main/resources/com/nt/throne/Audio/GameSong/blockImpactSound.mp3").toURI().toString()));
         this.background = background;
         structures = new CopyOnWriteArrayList<>();
         enemies = new CopyOnWriteArrayList<>();
         bullets = new CopyOnWriteArrayList<>();
         guns = new CopyOnWriteArrayList<>();
         random = new Random();
-
         bullets = new CopyOnWriteArrayList<>();
         limitX = new int[2];
         limitY = new int[2];
@@ -67,8 +74,10 @@ public abstract class Scenario extends BaseScreen {
         if (areGunsGenerated) for (Gun gun : guns) gun.paint(graphicsContext);
         if (hero.getActualGun() != null) hero.getActualGun().paint(graphicsContext);
         for (Enemy enemy : enemies) {
+            if (enemy instanceof ShooterEnemy) {
+                ((ShooterEnemy) enemy).getActualGun().paint(graphicsContext);
+            }
             enemy.paint(graphicsContext);
-            ((ShooterEnemy) enemy).getActualGun().paint(graphicsContext);
         }
         run();
     }
@@ -78,9 +87,12 @@ public abstract class Scenario extends BaseScreen {
         bullets.removeIf(this::bulletsLogic);
         if (movingEnemies) {
             for (Enemy enemy : enemies) {
-                if (enemy instanceof ShooterEnemy) {
-                    ((ShooterEnemy) enemy).setFocus(hero.getPosition());
-                    ((ShooterEnemy) enemy).moveAndShot(hero.getPrefferedArea(), getBullets());
+                if (enemy instanceof ShooterEnemy shooter) {
+                    shooter.setFocus(hero.getPosition());
+                    shooter.moveAndShot(hero.getPrefferedArea(), getBullets());
+                }
+                if(enemy instanceof ChaserEnemy chaser) {
+                    chaser.calculateMovement();
                 }
             }
         }
@@ -88,20 +100,20 @@ public abstract class Scenario extends BaseScreen {
 
     private boolean bulletsLogic(Bullet bullet) {
         boolean ans = !isInBounds(bullet);
-        if (enemies.size() > 0) {
-            for (int i = 0; i < enemies.size(); i++) {
-                if (bullet.isHurting(enemies.get(i))) {
-                    enemies.get(i).takeDamage(bullet);
-                    if (enemies.get(i).getLife() <= 0) {
-                        enemies.remove(enemies.get(i));
-                    }
-                    ans = true;
+        for (Enemy enemy : enemies) {
+            if (bullet.isHurting(enemy)) {
+                bodyImpactSound.play();
+                enemy.takeDamage(bullet);
+                if (enemy.getLife() <= 0) {
+                    enemies.remove(enemy);
                 }
+                ans = true;
             }
         }
 
         for (Structure structure : structures) {
             if (bullet.isHurting(structure)) {
+                blockImpactSound.play();
                 structure.takeDamage(bullet);
                 if (structure.getLife() <= 0) {
                     structures.remove(structure);
@@ -159,6 +171,10 @@ public abstract class Scenario extends BaseScreen {
     public void gunsLogic() {
         for (Gun gun : guns) {
             if (hero.isColliding(gun)) {
+                if (hero.getActualGun() != null) {
+                    hero.getActualGun().setPosition(hero.getPosition());
+                    guns.add(hero.getActualGun());
+                }
                 hero.setActualGun(gun);
                 guns.remove(gun);
             }
@@ -192,21 +208,46 @@ public abstract class Scenario extends BaseScreen {
     public void onMousePressed(MouseEvent event) {
         shooting = true;
         new Thread(() -> {
+            makeSound();
             while (shooting) {
                 shoot(event);
-
                 try {
                     Thread.sleep(150);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
+                hero.getActualGun().getShotSound().stop();
+                hero.getActualGun().getShotSound().seek(Duration.ZERO);
             }
+        }).start();
+    }
+
+    public void makeSound() {
+        new Thread(() -> {
+            while (shooting) {
+                hero.getActualGun().getShotSound().play();
+                try {
+                    Thread.sleep((long) hero.getActualGun().getShotSound().getCycleDuration().toMillis());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            hero.getActualGun().getShotSound().stop();
+            hero.getActualGun().getShotSound().seek(Duration.ZERO);
         }).start();
     }
 
     public void shoot(MouseEvent event) {
         if (hero.getActualGun() != null) {
+            hero.getActualGun().getShotSound().play();
             hero.getActualGun().onShot(bullets, new Point2D(event.getX(), event.getY()));
+        }
+        try {
+            Thread.sleep(150);
+            hero.getActualGun().getShotSound().stop();
+            hero.getActualGun().getShotSound().seek(Duration.ZERO);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -228,6 +269,8 @@ public abstract class Scenario extends BaseScreen {
     @Override
     public void onMouseReleased(MouseEvent event) {
         shooting = false;
+        hero.getActualGun().getShotSound().stop();
+        hero.getActualGun().getShotSound().seek(Duration.ZERO);
     }
 
     @Override
